335899d13a388bfb747ae67a87207cb18be4fa90
brunopaiva1 <brunopaivamaster10@gmail.com>
2025-06-03 15:46:41 -0300

    add settings from ask eight MPI

--- Q8/main.c
+++ Q8/main.c
@@ -6,14 +6,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <math.h>
 
-int main(int argc, char argv){
-    int rank;
-    int comm_sz;
+int main(int argc, char* argv[]) {
+    int comm_sz; 
+    int rank; 
+    int local_value = 0;
+    int global_sum = 0; 
 
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    local_value = rank;
+    global_sum = local_value;
+
+    int pd2 = 1;
+    while (pd2 * 2 <= comm_sz) {
+        pd2 *= 2;
+    }
+
+    if (rank >= pd2) {
+        int partner = rank - pd2; 
+        MPI_Send(&global_sum, 1, MPI_INT, partner, 0, MPI_COMM_WORLD);
+    }
+    else {
+        if (rank + pd2 < comm_sz) {
+            int partner = rank + pd2;
+            int received_value;
+            MPI_Recv(&received_value, 1, MPI_INT, partner, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            global_sum += received_value; 
+        }
+    }
+
+    if (rank < pd2) {
+        for (int i = 0; i < (int)log2(pd2); i++) { 
+            int partner;
+            int mask = 1 << i;
+
+            if ((rank & mask) == 0) { 
+                partner = rank | mask;
+                if (partner < pd2) { 
+                    int received_value;
+                    MPI_Recv(&received_value, 1, MPI_INT, partner, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    global_sum += received_value;
+                }
+            } else { 
+                partner = rank ^ mask;
+                MPI_Send(&global_sum, 1, MPI_INT, partner, 0, MPI_COMM_WORLD);
+                break;
+            }
+        }
+    }
+
+    if (rank == 0) {
+        printf("Soma global final: %d\n", global_sum);
+    }
+
+    MPI_Finalize();
     return 0;
 }
