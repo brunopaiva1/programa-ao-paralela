5aab9bfd7c21ab204a96e75d381b7c111c2273b6
brunopaiva1 <brunopaivamaster10@gmail.com>
2025-07-29 14:46:03 -0300

    ajuste de caminho para executar no sdumont

--- CUDA/Trapezio/run.sh
+++ CUDA/Trapezio/run.sh
@@ -1,4 +1,5 @@
-#SBATCH --nodes=4
+#!/bin/bash
+#SBATCH --nodes=1
 #SBATCH --time=00:10:00
 #SBATCH --cpus-per-task=24
 #SBATCH -p sequana_cpu_dev
@@ -7,7 +8,7 @@
 
 echo $SLUM_JOB_NODELIST
 
-cd /scratch/pex1272-ufersa/bruno.silva7/Trapezio
+cd /scratch/pex1272-ufersa/bruno.silva7/programacao-paralela/CUDA/Trapezio
 
 module load gcc/14.2.0_sequana
 module load openmpi/gnu/4.1.4+gcc-12.4+cuda-11.6_sequana
