10ddc225ab203a59e54ee3b277790c5fb9cabb77
brunopaiva1 <brunopaivamaster10@gmail.com>
2025-08-08 22:59:59 -0300

    add time para bitonic sort sdumont

--- CUDA/Bitonic_sort/run.sh
+++ CUDA/Bitonic_sort/run.sh
@@ -6,11 +6,11 @@
 #SBATCH -J exemplo
 #SBATCH --exclusive
 
-echo $SLUM_JOB_NODELIST
+echo $SLUM_JOB_NODELIST ; wait
 
-cd /scratch/pex1272-ufersa/bruno.silva7/programacao-paralela/CUDA/Bitonic_sort
+cd /scratch/pex1272-ufersa/bruno.silva7/programacao-paralela/CUDA/Bitonic_sort ; wait
 
-module load gcc/14.2.0_sequana
-module load openmpi/gnu/4.1.4+gcc-12.4+cuda-11.6_sequana
+module load gcc/14.2.0_sequana ; wait
+module load openmpi/gnu/4.1.4+gcc-12.4+cuda-11.6_sequana ; wait
 
 time ./bitonic_sort.exe 16
